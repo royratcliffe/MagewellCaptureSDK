19f66b1b7a6682cb73d693e1ecc4dfddaa72189a
Roy Ratcliffe <roy@ratcliffe.me>
2026-01-14 10:32:00 +0000

    refactor: update comments for MWGetVersion usage
    Enhance comments in the get_version function to provide
    context on the MWGetVersion function's behaviour and its
    return value handling.

--- src/MagewellCapture.cpp
+++ src/MagewellCapture.cpp
@@ -8,8 +8,10 @@ using namespace magewell_capture;
 uint32_t magewell_capture::get_version() {
   BYTE major = 0, minor = 0;
   WORD build = 0;
-  // At version 3.3.1
-  MWGetVersion(&major, &minor, &build);
+  // At version 3.3.1, the function MWGetVersion is a static inline
+  // function defined in MWCapture.h; it always answers MW_SUCCEEDED.
+  // Ignore the return value.
+  (void)MWGetVersion(&major, &minor, &build);
   return (static_cast<uint32_t>(major) << 24) |
          (static_cast<uint32_t>(minor) << 16) | static_cast<uint32_t>(build);
 }
