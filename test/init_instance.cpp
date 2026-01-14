313e6212d761b14705cf8d2ff3401fb1dc0803b3
Roy Ratcliffe <roy@ratcliffe.me>
2026-01-14 13:14:59 +0000

    test: verify idempotency of init_instance calls
    Add additional calls to magewell_capture::init_instance in the
    test_init_instance function to ensure that the function behaves
    as expected when invoked multiple times.

--- test/init_instance.cpp
+++ test/init_instance.cpp
@@ -6,6 +6,9 @@ int test_init_instance(int argc, char *argv[]) {
   (void)argc;
   (void)argv;
 
+  // Call init_instance multiple times to verify idempotency.
+  magewell_capture::init_instance();
+  magewell_capture::init_instance();
   magewell_capture::init_instance();
   return EXIT_SUCCESS;
 }
